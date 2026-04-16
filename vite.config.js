2a748bdb38094523386978bf97efe977be8ceef6
zaelrios <53027991+zaelrios@users.noreply.github.com>
2026-04-16 16:27:01 +0000

    app2

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // <-- AQUÍ REGRESAMOS EL DISEÑO
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
+    tailwindcss(), // <-- AQUÍ LO ACTIVAMOS DE NUEVO
     react(),
     VitePWA({
-      registerType: 'autoUpdate', // Se actualiza sola en los celulares cuando subes cambios
+      registerType: 'autoUpdate', 
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.png'],
       manifest: {
         name: 'Ventaja Adentro',
         short_name: 'VAd.',
         description: 'Matchmaking y Ranking ELO para Tenis',
-        theme_color: '#29C454', // El verde de la app para la barra superior
-        background_color: '#F8F7F2', // El color hueso para la pantalla de carga
-        display: 'standalone', // Esto es lo que quita la barra del navegador en el cel
-        orientation: 'portrait', // Para que no se voltee si acuestan el celular
+        theme_color: '#29C454', 
+        background_color: '#F8F7F2', 
+        display: 'standalone', 
+        orientation: 'portrait', 
         icons: [
           {
             src: 'logo192.png',
